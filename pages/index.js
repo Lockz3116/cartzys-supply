8a43a95015103aa446c167ed3e358f9edaed3727
Bytee <lockieoc@gmail.com>
2025-12-02 20:04:54 +1300

    Delete pages directory

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-const CartzysPage = dynamic(() => import("../src/CartzysPage"), { ssr: false });
-
-export default function Home() {
-  return <CartzysPage />;
-}
